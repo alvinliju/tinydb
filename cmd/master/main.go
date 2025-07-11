53a92990ba03f3061f3bd07f47c8f5ea72e7234e
lemon <l3mon@lemons-MacBook-Air.local>
2025-07-11 16:25:26 +0530

    feat:Master server with volume routing - PUT/GET/DELETE working

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -6,16 +6,37 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 // now our master gets a put requests and
 // it forwards it to the volume server and return something
 var httpClient *http.Client
 
+var db *leveldb.DB
+
+var volumeServers = []string{
+	"http://localhost:3001",
+}
+
+func selectVolumeServer() string {
+	// Simple round-robin for now
+	return volumeServers[time.Now().UnixNano()%int64(len(volumeServers))]
+}
+
 func init() {
 	httpClient = &http.Client{
 		Timeout: 10 * time.Second,
 	}
+
+	var err error
+	db, err = leveldb.OpenFile("./tinydb_master", nil)
+	if err != nil {
+		fmt.Println("Error connecting leveldb", err)
+		return
+	}
+
 }
 
 func main() {
@@ -51,8 +72,9 @@ func handlePut(w http.ResponseWriter, r *http.Request) {
 	//we recieve the request right?
 	// we have the key and we have the data in the request body
 	//create me put request and send the data to the volume shit
-	volumeServer := "http://localhost:3001/files/" + key
-	request, err := http.NewRequest("PUT", volumeServer, r.Body)
+	volumeServer := selectVolumeServer()
+	redirectURI := volumeServer + "/files/" + key
+	request, err := http.NewRequest("PUT", redirectURI, r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -61,8 +83,7 @@ func handlePut(w http.ResponseWriter, r *http.Request) {
 	client := httpClient
 	resp, err := client.Do(request)
 	if err != nil {
-		log.Printf("Master: Error sending PUT request to volume server %s: %v", volumeServer, err)
-		// A 502 Bad Gateway is appropriate if the upstream server (Volume Server) is unreachable or errors out.
+		log.Printf("Master: Error sending PUT request to volume server %s: %v", redirectURI, err)
 		http.Error(w, "Failed to store file: volume server unreachable or error", http.StatusBadGateway)
 		return
 	}
@@ -74,6 +95,10 @@ func handlePut(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = db.Put([]byte(key), []byte(volumeServer), nil)
+	if err != nil {
+		http.Error(w, "Error saving key to master", http.StatusInternalServerError)
+	}
 	fmt.Printf("Here is the key %s", string(volumeRespBody))
 	w.WriteHeader(http.StatusCreated)
 }
@@ -86,11 +111,20 @@ func handleGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//TODO check if the key exists in our master
+	volumeServer, err := db.Get([]byte(key), nil)
+	fmt.Println(string(volumeServer))
+	if err != nil {
+		if err == leveldb.ErrNotFound {
+			fmt.Println(err)
+			http.Error(w, "key not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
 
-	volumeServer := "http://localhost:3001/files/" + key
-
-	http.Redirect(w, r, volumeServer, http.StatusMovedPermanently)
-
+	redirectURI := string(volumeServer) + "/files/" + key
+	http.Redirect(w, r, string(redirectURI), http.StatusMovedPermanently)
 }
 
 func handleDelete(w http.ResponseWriter, r *http.Request) {
@@ -101,8 +135,18 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//TODO:ping volumes and load pick a random one and store to keyvalue store
-	volumeServer := "http://localhost:3001/files/" + key
-	request, err := http.NewRequest("DELETE", volumeServer, r.Body)
+	volumeServer, err := db.Get([]byte(key), nil)
+	redirectURI := string(volumeServer) + "/files" + key
+	fmt.Println(string(volumeServer))
+	if err != nil {
+		if err == leveldb.ErrNotFound {
+			http.Error(w, "key not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+	request, err := http.NewRequest("DELETE", string(redirectURI), r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -111,7 +155,7 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 	client := httpClient
 	resp, err := client.Do(request)
 	if err != nil {
-		log.Printf("Master: Error sending PUT request to volume server %s: %v", volumeServer, err)
+		log.Printf("Master: Error sending DEL request to volume server %s: %v", redirectURI, err)
 		// A 502 Bad Gateway is appropriate if the upstream server (Volume Server) is unreachable or errors out.
 		http.Error(w, "Failed to store file: volume server unreachable or error", http.StatusBadGateway)
 		return
@@ -129,6 +173,12 @@ func handleDelete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("Here is the key %s", string(volumeRespBody))
+	err = db.Delete([]byte(key), nil)
+	if err != nil {
+		http.Error(w, "Database Error", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Printf(" %s Deleted", string(volumeRespBody))
 	w.WriteHeader(http.StatusCreated)
 }
